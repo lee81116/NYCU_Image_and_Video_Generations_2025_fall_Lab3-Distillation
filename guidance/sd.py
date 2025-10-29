70702236c7dedf86a0b7b92992c8eebd9f2ed10c
Nian-Chia <103125276+lee81116@users.noreply.github.com>
2025-10-30 01:38:46 +0800

    Update sd.py

--- guidance/sd.py
+++ guidance/sd.py
@@ -173,7 +173,7 @@ class StableDiffusion(nn.Module):
         tt = torch.cat([t] * 2)
         lora_pred = self.unet(latent_model_input, tt, encoder_hidden_states=text_embeddings).sample
         lora_pred = latent_model_input * torch.cat([w] * 2, dim=0).view(-1, 1, 1, 1) + noise_pred * torch.cat([alpha_t] * 2, dim=0).view(-1, 1, 1, 1)
-        dump, lora_pred = lora_pred.chunk
+        dump, lora_pred = lora_pred.chunk(2)
 
         lora_pred = alpha_t * lora_pred
         target = alpha_t * eps
